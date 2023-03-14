2ffa89a48d55b7086418141684da1ca0c16ab567
unknown <mohdaazen@google.com>
2023-03-14 14:08:32 +0530

    added content box to write about the topic/day and added for loop in new style so that all the inputs gets logged through the home directory of the project

--- app.js
+++ app.js
@@ -16,8 +16,16 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
+
+
+
 app.get("/",function(req,res){
-  res.render("home",{StartingContent: homeStartingContent });
+  res.render("home",{
+    StartingContent: homeStartingContent,
+    newPosts: posts
+  });
+    // console.log(posts);
 });
 
 
@@ -39,10 +47,12 @@ app.post("/compose",function(req,res){
   // console.log(req.body.postTitle);
   // console.log(req.body.postBody);
 
-  const post{
+  const post = {
     title: req.body.postTitle,
     content: req.body.postBody
   };
+  posts.push(post);
+  res.redirect("/");
 });
 
 
